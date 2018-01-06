179b4e2b960ce5a074ef30bd08572aa20845bf64
interbiznw <j.owens@interbiznw.com>
2018-01-05 17:07:58 -0800

    Update zclassic_explorer_2.sh

--- zclassic_explorer_2.sh
+++ zclassic_explorer_2.sh
@@ -77,4 +77,5 @@ EOF
 
 echo "Start the block explorer, open in your browser http://server_ip"
 echo "if this does not work and gives an error due to port 80 you can change the port or run with escilated priviliges"
+echo "Run the command on the following line to start the block explorer"
 echo "nvm use v4; ./zclassic-explorer/node_modules/bitcore-node-zcash/bin/bitcore-node start"
